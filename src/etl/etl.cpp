7657c54b4dfb34a9e945a1c9bd426952a072e967
ksharma6 <ksharma6@tutanota.com>
2023-10-04 17:12:32 -0700

    Added read_csv and csv_to_eigen functions

--- src/etl/etl.cpp
+++ src/etl/etl.cpp
@@ -6,43 +6,54 @@
 #include "../lib/eigen/Eigen/Dense"
 #include "etl.h"
 
-
-
-
-ETL::ETL(std::string a, char b, bool c):path(a),sep(b),header(c)
+ETL::ETL(std::string a, char b, bool c, int d, int e):path(a),sep(b),header(c), rows(d), cols(e)
 {}
 
 
-std::vector<std::vector <char>> ETL::load_data()
-    {
-        std::vector<std::vector <char>> datastring;
-        
-        std::fstream raw_data(path, std::ios::in);
+std::vector<std::vector <std::string>> ETL::read_csv()
+    {   
         std::string line;
-        
-        while(getline(raw_data, line, sep))
-            {
-                std::vector<char> row;
-                
-                for (char &c : line)
-                {
-                    if (c != sep)
-                    {
-                        row.push_back(c);
-                    }
-                }
-                datastring.push_back(row);
-            }
-        
+        int r = 0;
 
-        for ( std::vector<char> &row : datastring)
+        std::vector <std::vector <std::string>> csv;
+        std::ifstream datafile(path);
+        while (r < rows && getline(datafile, line))
         {
-            for (char &c : row)
-            {
-                std::cout << c<< ' ';
-            }
-            std::cout << '\n';
-        }
+            std::vector<std::string> row;
 
-        return datastring;
+            std::stringstream iss(line);
+            int c= 0;
+            std::string val;
+
+            while (c < cols && getline(iss, val, sep))
+            {
+                row.push_back(val);
+            }
+            csv.push_back(row);
+        }
+        return csv;
+
     };
+
+Eigen::MatrixXd ETL::csv_to_eigen(std::vector<std::vector <std::string>> dataset)
+{
+    
+    rows =  dataset.size();
+    cols = dataset[0].size();
+    if (header == true)
+    {
+        rows = rows -1;
+    }
+    
+    Eigen::MatrixXd matrix(rows, cols);
+
+    for (int i =0; i < rows; i++)
+    {
+        for (int j =0; j < cols; j++)
+        {
+            matrix(i, j) = std::stod(dataset[i][j].c_str());
+        }
+    }
+    return matrix;
+}
+
--- src/etl/etl.h
+++ src/etl/etl.h
@@ -5,15 +5,35 @@
 #include <string>
 #include <vector>
 
+/**
+ * ETL class extracts, transforms and loads dataset
+ * 
+ * Class params:
+ *  - string path: path to data 
+ *  - char sep: seperator in data file for parsing 
+ *  - bool header: indicates whethere data contains a column header or not.
+ * 
+*/
 class ETL
 {
 public:
     std::string path;
     char sep;
     bool header;
+    int rows;
+    int cols;
+
     
-    ETL(std::string a, char b, bool c);
-    std::vector<std::vector <char>> load_data();
+    ETL(std::string a, char b, bool c, int d, int e);
+    /*
+    load dataset into memory and return 2D vector
+    */
+    std::vector<std::vector <std::string>> read_csv();
+    /*
+    convert 2D vector into 2d eigen matrix
+    */
+    Eigen::MatrixXd csv_to_eigen(std::vector<std::vector <std::string>> dataset);
+
 
 
 
